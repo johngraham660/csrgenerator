dbffbfd1bcc6489bbc5782646f4e80852416f72a
John Graham <john.graham@virtua-enterprise.co.uk>
2018-09-07 19:12:16 +0100

    Added some commentry

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def generate_spark_request():
     '''
     Intention here is to get the CSR and submit a request into Spark.
     We would then return the Spark REQ number to the user
+
+    NOTE: None of this is working - DO NOT USE
     '''
     apikey = 'insert-api-key'
     response = 'get spark response'
